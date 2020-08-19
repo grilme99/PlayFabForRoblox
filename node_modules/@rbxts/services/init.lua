f3e38474c732666b52c7df03e52212f2b0633d02
Brooke Rhodes <podcast46231@gmail.com>
2020-08-19 21:15:05 +0100

    Remove files

--- node_modules/@rbxts/services/init.lua
+++
@@ -1,7 +0,0 @@
-return setmetatable({}, {
-    __index = function(self, serviceName)
-        local service = game:GetService(serviceName);
-        self[serviceName] = service;
-        return service;
-    end;
-})
