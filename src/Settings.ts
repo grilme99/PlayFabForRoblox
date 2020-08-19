226957bd8ed39f2bc7cfc9b1641393740ce75f5c
Brooke Rhodes <podcast46231@gmail.com>
2020-08-19 17:08:10 +0100

    Update settings

---
+++ src/Settings.ts
@@ -0,0 +1,15 @@
+let settings = {
+    _internalSettings: {
+        sdkVersionString: 'RobloxSdk_0.1.0',
+        requestGetParams: {['sdk']: 'LuaSdk_0.83.200805'},
+        productionUrl: '.playfabapi.com'
+    },
+
+    settings: {
+        titleId: '',
+        secretKey: '',
+        entityToken: ''
+    }
+}
+
+export default settings
