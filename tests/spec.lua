b6ed0ee8e5654e45c3f0a1d09fe695acd2741448
Brooke Rhodes <podcast46231@gmail.com>
2020-09-18 17:39:29 +0100

    Update test place project file and spec.lua

--- tests/spec.lua
+++ tests/spec.lua
@@ -1,15 +1,6 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-local TS = require(ReplicatedStorage.rbxts_include.RuntimeLib)
-local TestEZ = require(ReplicatedStorage.rbxts_include.node_modules.testez.src)
-
+local TestEZ = require(ReplicatedStorage.TestEZ)
 local testsFolder = ReplicatedStorage.TS
-
-for _, script in pairs(testsFolder:GetDescendants()) do
-	if script.Name:match(".spec") then
-		_G[script] = TS
-	end
-end
 
 local results = TestEZ.TestBootstrap:run({testsFolder}, TestEZ.Reporters.TextReporter)
 
