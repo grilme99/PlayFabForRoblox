98a453e6e2b28adb504347d8b7d79a0b1d36c63a
Brooke Rhodes <podcast46231@gmail.com>
2020-09-18 18:06:24 +0100

    spec.server.lua

--- src/tests/spec.lua
+++
@@ -1,9 +0,0 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local TestEZ = require(ReplicatedStorage.TestEZ)
-local testsFolder = ReplicatedStorage.TS
-
-local results = TestEZ.TestBootstrap:run({testsFolder}, TestEZ.Reporters.TextReporter)
-
-if #results.errors > 0 or results.failureCount > 0 then
-	error("Tests failed")
-end
