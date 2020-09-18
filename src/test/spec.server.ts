6f9bc003cfec5b6206e6ea4183f0d844ae4ba674
Brooke Rhodes <podcast46231@gmail.com>
2020-09-18 18:14:40 +0100

    Remove tests for now

--- src/test/spec.server.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable prettier/prettier */
-import TestEZ from '@rbxts/testez'
-const ReplicatedStorage = game.GetService('ReplicatedStorage')
-
-const results = TestEZ.TestBootstrap.run([ReplicatedStorage], TestEZ.Reporters.TextReporter)
-if (results.errors.size() > 0 || results.failureCount > 0) {
-	error('Tests failed')
-}
